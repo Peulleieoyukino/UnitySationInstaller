8ac6f729b5b510c18110653a5d8f4303e55567d1
Peulleieo Yukino <Peulleieoyukino@users.noreply.github.com>
2022-02-04 00:38:05 +0100

    v0.2
    Minor update of all contents

--- UnityStationInstaller.sh
+++ UnityStationInstaller.sh
@@ -3,11 +3,11 @@ apt full upgrade
 apt dist-upgrade
 apt install -y wget p7zip
 mkdir /usr/share/Unitystation/
+cd /usr/share/Unitystation/
 wget https://github.com/unitystation/stationhub/releases/download/930/lin930.zip -O /usr/share/Unitystation/lin-lastest.zip 
-unzip *.zip /usr/share/Unitystation/lin-lastest.zip
-chmod -R 750 /usr/share/Unitystation/StationHub
-wget https://github.com/unitystation/stationhub/blob/develop/UnitystationLauncher/Assets/unityico.png -O /user/share/Unitystation/unityico.png
+wget https://raw.githubusercontent.com/unitystation/stationhub/develop/UnitystationLauncher/Assets/unityico.png -O /user/share/Unitystation/unityico.png
 wget https://raw.githubusercontent.com/Peulleieoyukino/UnitySationInstaller/main/Unitystation.desktop -O /usr/share/applications/Stationhub.desktop
-rm -rfv *.zip /usr/share/Unitystation/
- 
-
+unzip /usr/share/Unitystation/lin-lastest.zip
+rm -rfv /usr/share/Unitystation/lin-lastest.zip
+chmod -R 777 /usr/share/Unitystation/StationHub 
+exit
