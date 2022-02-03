32ac8c6a52c785caeb4ec021735592dcbd51168e
Peulleieo Yukino <Peulleieoyukino@users.noreply.github.com>
2022-02-03 23:52:16 +0100

    v0.1.3
    Changed URL again

--- UnityStationInstaller.sh
+++ UnityStationInstaller.sh
@@ -7,7 +7,7 @@ wget https://github.com/unitystation/stationhub/releases/download/930/lin930.zip
 unzip *.zip /usr/share/Unitystation/lin-lastest.zip
 chmod -R 750 /usr/share/Unitystation/StationHub
 wget https://github.com/unitystation/stationhub/blob/develop/UnitystationLauncher/Assets/unityico.png -O /user/share/Unitystation/unityico.png
-wget https://github.com/Peulleieoyukino/UnitySationInstaller/Unitystation.desktop -O /usr/share/applications/Stationhub.desktop
+wget https://raw.githubusercontent.com/Peulleieoyukino/UnitySationInstaller/main/Unitystation.desktop -O /usr/share/applications/Stationhub.desktop
 rm -rfv *.zip /usr/share/Unitystation/
  
 
