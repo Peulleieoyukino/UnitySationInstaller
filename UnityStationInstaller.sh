86491127c823a38e33dfce5ecb038342d875b157
Peulleieo Yukino <Peulleieoyukino@users.noreply.github.com>
2022-02-03 23:43:11 +0100

    v0.1.2

--- UnityStationInstaller.sh
+++ UnityStationInstaller.sh
@@ -5,9 +5,9 @@ apt install -y wget p7zip
 mkdir /usr/share/Unitystation/
 wget https://github.com/unitystation/stationhub/releases/download/930/lin930.zip -O /usr/share/Unitystation/lin-lastest.zip 
 unzip *.zip /usr/share/Unitystation/lin-lastest.zip
-chmod -R 750 ./StationHub
+chmod -R 750 /usr/share/Unitystation/StationHub
 wget https://github.com/unitystation/stationhub/blob/develop/UnitystationLauncher/Assets/unityico.png -O /user/share/Unitystation/unityico.png
-wget https://github.com/Peulleieoyukino/UnitySationInstaller/Stationhub.desktop -O /usr/share/applications/Stationhub.desktop
+wget https://github.com/Peulleieoyukino/UnitySationInstaller/Unitystation.desktop -O /usr/share/applications/Stationhub.desktop
 rm -rfv *.zip /usr/share/Unitystation/
  
 
